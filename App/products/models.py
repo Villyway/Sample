b6ba2560a371ee4f45273ecb88fc352b62a6bcb4
rupesh7399 <rupesh7399@gmail.com>
2023-08-29 12:33:08 +0530

    Branch:master->add Product models in Unit, Product, Attribute, product attribute, attribute

--- App/products/models.py
+++ App/products/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
+from base.models import Base
+
 # Create your models here.
+# Unit
+class Unit(Base):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+
+#Product
+class Product(Base):
+    code = models.CharField(max_length=20)
+    
+    description = models.TextField(blank=True, null=True)
+    umo = models.ForeignKey(
+        Unit, on_delete=models.CASCADE, related_name='product_unit',)
+    specification = models.TextField(blank=True, null=True)
+
+
+# Model for Attribute
+class Attribute(Base):
+    name = models.CharField("Name", max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+# Model for Property
+class ProductAttribute(Base):
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='product_property',)
+    attribute = models.ForeignKey(
+        Attribute, on_delete=models.SET_NULL, null=True, related_name='property_attribute')
+    value = models.CharField(max_length=150)
+
+    # objects = AttributeManager()
+
+    def __str__(self):
+        return self.attribute.name
+
+    
