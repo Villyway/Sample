cfcf74fcb7bc32072170db27c17f482d3ec79c65
rupesh7399 <rupesh7399@gmail.com>
2023-08-29 11:50:06 +0530

    Branch:master -> add role in user model.

--- App/users/models.py
+++ App/users/models.py
@@ -10,8 +10,8 @@ class User(AbstractUser):
     name = models.CharField(max_length=30)
     mobile = models.CharField(max_length=15, null=True, blank=True)
     country_code = models.CharField(max_length=4, default="91")
-    # role = models.CharField(
-    #     max_length=25, choices=Roles.choices(), default=Roles.OPERATOR.value)
+    role = models.CharField(
+        max_length=25, choices=Roles.choices(), default=Roles.OPERATOR.value)
     otp = models.CharField(max_length=4, null=True, blank=True)
     otp_created_at = models.DateTimeField(blank=True, null=True)
     is_deactivated = models.BooleanField(default=False)
