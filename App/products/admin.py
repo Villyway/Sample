4d884c13c5acdfe98daabdf49555cbb57bed913b
rupesh7399 <rupesh7399@gmail.com>
2023-08-29 14:02:49 +0530

    Branch:master-> add admin in product and vendor.

--- App/products/admin.py
+++ App/products/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product, Attribute, ProductAttribute, Unit
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["code","umo","stock","minimum_stock_level", "rack_no","tray_no" ]
+
+
+@admin.register(Attribute)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(ProductAttribute)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["product","attribute","value"]
+
+
+@admin.register(Unit)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
