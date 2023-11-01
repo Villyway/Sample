847149ed11989e14fa9821f39ca014eced39415c
rupesh7399 <rupesh7399@gmail.com>
2023-11-01 17:47:31 +0530

    Branch:INV003->add order modules.

---
+++ App1.1/orders/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from django.contrib.auth.decorators import login_required
+
+from .views import (InwardCreateView,
+                    OutwardCreateView, Dashboard, 
+                    ExportData, GetBillNoByInword, QCView, 
+                    SimpleAddStock, StockHistoryInJson, StockHistoriesList, 
+                    InventryReport, InventryReportStock)
+
+app_name = "inventry"
+
+urlpatterns = [
+    path("inward/", login_required(InwardCreateView.as_view()),
+         name="inward"),
+    path("outward/", login_required(OutwardCreateView.as_view()),
+         name="outward"),
+    path("dashboard",login_required(Dashboard.as_view()), name="inventry-dashboard"),
+    path("<slug:id>/bill-inword",login_required(GetBillNoByInword.as_view()), name="bill-inword"),
+    path("qc-list/",login_required(QCView.as_view()), name="qc-list"),
+    path("add-stock/", login_required(SimpleAddStock.as_view()), name="add-stock"),
+    path("<slug:id>/stock-history/", login_required(StockHistoryInJson.as_view()), name="json-stock-history"),
+    path("stock-history/", login_required(StockHistoriesList.as_view()), name="stock-histories"),
+    path("reports/", login_required(InventryReport.as_view()), name="stock-report"),
+    path("export-report/",login_required(ExportData.as_view()), name="export-today"),
+    path("search-report/",login_required(InventryReportStock.as_view()), name="search-report"),
+
+]
--- App1.1/orders/views.py
+++ App1.1/orders/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from datetime import datetime
+
+from django.views.generic import View
+
+
+class Dashboard(View):
+    template_name = "orders/dashboard.html"
+
+    def get(self,request):
+
+        return render(request,self.template_name)
