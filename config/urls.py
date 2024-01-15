7eb98e2d77aa77851189f735ea009438302fbb24
rupesh7399 <rupesh7399@gmail.com>
2024-01-15 15:12:11 +0530

    set media files

--- config/urls.py
+++ config/urls.py
@@ -45,6 +45,8 @@ if settings.DEBUG:
                           document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
-
-urlpatterns += static(settings.MEDIA_URL,
+else:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
